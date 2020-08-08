781a133842004e4469f2d18e3ac4017e9b0cbe92
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-08 22:13:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,22 +3,22 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr2 = ["songs", "movies", "games", "poems"]
+  
 end
 
 def add_element_to_end_of_array(array, element)
-  arr = ["songs", "movies", "games", "poems"]
-  arr << "arrays!"
+
+  
 end
 
 def add_element_to_start_of_array(array, element)
-  arr = ["songs", "movies", "games", "poems"]
-  arr.unshift("wow")
+  
+  
 end
 
 def remove_element_from_end_of_array(array)
-  arr = ["songs", "movies", "games", "poems", "arrays!"]
-  arrays! = arr.pop
+  
+
 end
 
 def remove_element_from_start_of_array(array)
