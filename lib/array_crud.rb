2f8eef522bbbad474e0645d3c535e920f44cfcd2
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-08 22:25:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_teams << "arrays!"
-  puts famous_teams
+  
+  
 end
 
 def add_element_to_start_of_array(array, element)
